b9f693a332fbb2c63c43b4e21d1d6c894511e1a5
yGit741 <68815836+yGit741@users.noreply.github.com>
2020-10-23 19:54:09 +0300

    Add files via upload
    The first version of Exercise 1. pretty empty.

---
+++ hello_turtle.py
@@ -0,0 +1,13 @@
+#################################################################
+# FILE : hello_turtle.py
+# WRITER : your_name , your_login , your_id
+# EXERCISE : intro2cs2 ex1 2020
+# DESCRIPTION: A simple program that...
+# STUDENTS I DISCUSSED THE EXERCISE WITH: Bugs Bunny, b_bunny.
+#								 	      Daffy Duck, duck_daffy.
+# WEB PAGES I USED: www.looneytunes.com/lola_bunny
+# NOTES: ...
+#################################################################
+
+if __name__ == "__main__" :
+    
